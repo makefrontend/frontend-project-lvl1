c667a43ff75567d5040f4d87dcd246e803948810
Anton Rojdestvensky <makefrontend@ya.ru>
2020-08-23 23:55:03 +0300

    Step 8

--- src/games/progression.js
+++ src/games/progression.js
@@ -8,21 +8,20 @@ const rounds = [];
 const progressionList = [];
 
 const progressionGame = () => {
-  for (let i = 0; i < COUNT_GAME; i += 1) {
-    const firstValue = getRandomValue(0, 50);
-    const secondValue = getRandomValue(0, 50);
-    for (let k = 0; k < progressionLength; k += 1) {
-      const number = firstValue + secondValue * k;
-      progressionList.push(number);
-    }
-    const replaceProgression = getRandomValue(0, progressionList.length - 1);
-    const answer = progressionList[replaceProgression].toString();
-    progressionList[replaceProgression] = '..';
-    const question = progressionList.join(' ');
-    const round = { question, answer };
-    rounds.push(round);
+  const firstValue = getRandomValue(0, 50);
+  const secondValue = getRandomValue(0, 50);
+  for (let k = 0; k < progressionLength; k += 1) {
+    const number = firstValue + secondValue * k;
+    progressionList.push(number);
   }
+  const replaceProgression = getRandomValue(0, progressionList.length - 1);
+  const answer = progressionList[replaceProgression].toString();
+  progressionList[replaceProgression] = '..';
+  const question = progressionList.join(' ');
+  const round = { question, answer };
+  rounds.push(round);
   playGame(rounds, descriptionGame);
 };
+
 
 export default progressionGame;
