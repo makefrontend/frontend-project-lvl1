3d0ad815da9019cd0ef967e8be8c87989492e6e8
Anton Rojdestvensky <makefrontend@ya.ru>
2020-08-25 17:31:28 +0300

    Исправил логику игры, модульность, публикация пакета

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { isEven, getRandomValue } from '../src/utils.js';
-import COUNT_GAME from '../src/arguments.js';
+import startGameEven from '../src/games/even.js';
 
-let step = 1;
-const minValue = 0;
-const maxValue = 50;
-
-const playGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  while (step <= COUNT_GAME) {
-    const randomValue = getRandomValue(minValue, maxValue);
-    console.log(`Question: ${randomValue}`);
-    const getAnswerQuestion = readlineSync.question('Your answer: ');
-    if (getAnswerQuestion === isEven(randomValue)) {
-      console.log('"yes" is wrong answer ;(. Correct answer was "no"');
-      return;
-    }
-    console.log('Correct!');
-    step += 1;
-  }
-  console.log(`Congratulations, ${userName}`);
-};
-
-playGame();
+startGameEven();
---
+++ src/games/even.js
@@ -0,0 +1,21 @@
+import { isEven, getRandomValue } from '../utils.js';
+import COUNT_GAME from '../arguments.js';
+import { playGame } from '../index.js';
+
+const descriptionGame = 'Answer "yes" if the number is even, otherwise answer "no".'
+const minValue = 0;
+const maxValue = 50;
+
+const startGameEven = () => {
+  const rounds = [];
+  for (let i = 0; i < COUNT_GAME; i += 1) {
+    const randomValue = getRandomValue(minValue, maxValue);
+    const question = `${randomValue}`;
+    const answer = isEven(randomValue) ? 'yes' : 'no';
+    const round = { question, answer };
+    rounds.push(round);
+  }
+  playGame(rounds, descriptionGame);
+};
+
+export default startGameEven;
