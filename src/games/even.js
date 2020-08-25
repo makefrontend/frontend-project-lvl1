63c30acb2d718840ed85d708cc3ce15362b06a60
Anton Rojdestvensky <makefrontend@ya.ru>
2020-08-25 17:37:32 +0300

    исправление линтера

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import { isEven, getRandomValue } from '../utils.js';
 import COUNT_GAME from '../arguments.js';
 import { playGame } from '../index.js';
 
-const descriptionGame = 'Answer "yes" if the number is even, otherwise answer "no".'
+const descriptionGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 const minValue = 0;
 const maxValue = 50;
 
